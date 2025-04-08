cf17e683e10a9fc67d98abb440ace386d14bbfa1
LEXX <lexhutchzz@gmail.com>
2025-04-08 11:28:07 -0500

    Update route.ts

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+  apiVersion: "2024-04-10",
+});// api chnaged to 2024-04-10 from 2022-11-15
 
 export async function POST(req: Request) {
   console.log("API Route: STRIPE_SECRET_KEY starts with:", process.env.STRIPE_SECRET_KEY?.substring(0, 8));
